b71908b68f665e29bf5411b1f668912b3491aff6
chamo112 <chamo.011270@fmail.com>
2023-01-01 04:28:24 +0900

    :sparkles: GraphQLのデータ取得と画面表示

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,10 +1,32 @@
 import { Button } from 'ui';
+import { gql, useQuery } from "@apollo/client";
+
+const GET_ARTICLES = gql`
+    query GetArticles {
+        articles {
+            id
+            title
+            body
+        }
+    }
+`;
 
 export default function Web() {
+  const {loading, error, data} = useQuery(GET_ARTICLES);
+
+  if(loading) return 'loading...'
+  if(error) return 'Error!'
+
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      {data.articles.map((item) => {
+        return (
+          <div key={item.id}>
+            <div>{item.title}</div>
+            <div>{item.body}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
