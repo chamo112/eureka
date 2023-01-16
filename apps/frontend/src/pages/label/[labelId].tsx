1b07da80563e006d39a77c37f83521d16c33e4a1
chamo112 <chamo.011270@gmail.com>
2023-01-16 12:49:48 +0900

    :sparkles: Label Page追加

---
+++ apps/frontend/src/pages/label/[labelId].tsx
@@ -0,0 +1,39 @@
+import { useQuery } from '@apollo/client';
+import { useRouter } from 'next/router';
+
+import { graphql } from '@/gql';
+
+const queryDocument = graphql(`
+  query Articles($where: LabelWhereInput) {
+    articles {
+      id
+      title
+      body
+      labels(where: $where) {
+        id
+        name
+      }
+    }
+  }
+`);
+
+const LabelPage = () => {
+  const router = useRouter();
+  const { labelId } = router.query;
+  const { loading, error, data } = useQuery(queryDocument, {
+    variables: {
+      where: { id: typeof labelId === 'string' ? labelId : '' },
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {JSON.stringify(error)}</p>;
+  if (!data || !data.article) {
+    router.replace('/404');
+    return;
+  }
+
+  return <div></div>;
+};
+
+export default LabelPage;
