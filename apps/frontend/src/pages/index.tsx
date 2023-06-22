96ac925cf0c674d4b9a72bd40f527cadb00f7f4f
chamo112 <chamo.011270@gmail.com>
2023-06-23 00:31:56 +0900

    :tada: パスの修正

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from '@apollo/client';
+import Link from 'next/link';
 
 import { Button } from '@/components/Elements';
-import BlogArticleCards  from '@/features/blogs/components/BlogArticleCards';
+import BlogArticleCards from '@/features/blogs/components/BlogArticleCards';
 import { graphql } from '@/gql';
-
+import { pagePaths } from '@/utils/pagePaths';
 
 const queryDocument = graphql(`
   query MyQuery {
@@ -32,22 +33,27 @@ const Index = () => {
   if (!data) return <p>No Data!</p>;
 
   return (
-      <>
-        {/* TODO: レイアウト修正 */}
-        <Button>New Postaaa</Button>
+    <>
+      {/* TODO: レイアウト修正 */}
 
-        <div className='px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28'>
-          <div className='mb-10 md:mb-16'>
-            {/* ブログのタイトル */}
-            <h2 className='mb-4 text-center text-xl font-semibold text-gray-700 md:mb-6 lg:text-3xl'>
-              List of articles
-            </h2>
-          </div>
+      <div className='mt-6 flex justify-end pr-8'>
+        <Link href={pagePaths.newPost()}>
+          <Button>New Post</Button>
+        </Link>
+      </div>
 
-          {/* コンテンツ */}
-          <BlogArticleCards articles={data.articles} />
+      <div className='px-4 pb-20 pt-16 sm:px-6 lg:px-8 lg:pb-28 lg:pt-24'>
+        <div className='mb-10 md:mb-16'>
+          {/* ブログのタイトル */}
+          <h2 className='mb-4 text-center text-xl font-semibold text-gray-700 md:mb-6 lg:text-3xl'>
+            List of articles
+          </h2>
         </div>
-      </>
+
+        {/* コンテンツ */}
+        <BlogArticleCards articles={data.articles} />
+      </div>
+    </>
   );
 };
 export default Index;
