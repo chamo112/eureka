88a65ed00272c47ad77bbff164af754e7ea9a8cd
chamo112 <chamo.011270@gmail.com>
2023-06-26 08:27:46 +0900

    :recycle: UI修正

--- apps/frontend/src/features/blogs/components/BlogArticleCards.tsx
+++ apps/frontend/src/features/blogs/components/BlogArticleCards.tsx
@@ -25,28 +25,23 @@ type User = {
 
 const BlogArticleCards = (props: Props) => {
   return (
-    <div>
-      <>
-        <div className='mx-20 mt-12 grid gap-10 md:mx-auto md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+    <>
+      <div className='flex justify-center'>
+        <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:max-w-4xl md:grid-cols-3'>
           {props.articles.map((article) => (
             <ArticleCard
               key={article.id}
               id={article.id}
               title={article.title}
-              body={article.body}
               labels={article.labels}
               href='#'
               imageUrl='https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80'
-              author={{
-                name: `${article.createdBy!.name}`,
-                imageUrl: `${article.createdBy!.picture}`,
-              }}
               date={article.createdAt}
             />
           ))}
         </div>
-      </>
-    </div>
+      </div>
+    </>
   );
 };
 
--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -35,24 +35,17 @@ const Index = () => {
   return (
     <>
       {/* TODO: レイアウト修正 */}
-
-      <div className='mt-6 flex justify-end pr-8'>
+      <div className='mb-10 mt-6 flex justify-end pr-8'>
         <Link href={pagePaths.newPost()}>
           <Button>New Post</Button>
         </Link>
       </div>
 
-      <div className='px-4 pb-20 pt-16 sm:px-6 lg:px-8 lg:pb-28 lg:pt-24'>
-        <div className='mb-10 md:mb-16'>
-          {/* ブログのタイトル */}
-          <h2 className='mb-4 text-center text-xl font-semibold text-gray-700 md:mb-6 lg:text-3xl'>
-            List of articles
-          </h2>
-        </div>
+      {/* ブログのタイトル */}
+      <h2 className='mb-10 text-center text-3xl font-semibold text-gray-700'>Blog</h2>
 
-        {/* コンテンツ */}
-        <BlogArticleCards articles={data.articles} />
-      </div>
+      {/* コンテンツ */}
+      <BlogArticleCards articles={data.articles} />
     </>
   );
 };
